cce7cbc4bff30fa205ec1baca48292214ee09b1f
ViktoriaFomenko <94681488+ViktoriaFomenko@users.noreply.github.com>
2022-08-27 16:35:11 +0000

    Deploying to gh-pages from @ ViktoriaFomenko/goit-react-hw-05-movies@056984c50b01bcb61462aa51b7a7b31547f1dbe4 🚀

---
+++ static/js/156.c2ebdf21.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[156],{2156:function(e,t,r){r.r(t);var n=r(501),o=r(184);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h2",{children:"This page not found :("}),(0,o.jsx)(n.rU,{to:"/",children:"To Home page"})]})}}}]);
+//# sourceMappingURL=156.c2ebdf21.chunk.js.map
